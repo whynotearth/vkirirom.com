f99c1f2b04d48300277f5393c84d9d94b5c88d5e
Atharva Sharma <mailatharvasharma@gmail.com>
2019-06-06 19:08:11 +0530

    test2

--- pages/listing.vue
+++ pages/listing.vue
@@ -253,6 +253,7 @@
                 <p>Select Dates</p>
                 <v-text-field
                 type="text"
+                name="Date"
                 id="datepicker"
                 placeholder="Select dates"
                 :value="formatDates(dateOne, dateTwo)"
@@ -369,15 +370,15 @@
               </v-flex>
               <v-flex xs12>
               <p>Select Dates</p>
-              <!-- <v-btn dark color="cyan darken-4" id="datepicker-trigger" block> -->
+              <v-btn dark color="cyan darken-4" id="datepicker-trigger" block>
                 <input
                 type="date"
                 id="datepicker-trigger"
                 placeholder="Select dates"
                 :value="formatDates(dateOne, dateTwo)"
                 >
-              <!-- </v-btn> -->
-              <!-- <v-text-field :value="formatDates(dateOne, dateTwo)" hidden></v-text-field>
+              </v-btn>
+              <v-text-field :value="formatDates(dateOne, dateTwo)" hidden></v-text-field>
               <AirbnbStyleDatepicker
                 :trigger-element-id="'datepicker-trigger'"
                 :mode="'range'"
@@ -386,7 +387,7 @@
                 :date-two="dateTwo"
                 @date-one-selected="val => { dateOne = val }"
                 @date-two-selected="val => { dateTwo = val }"
-              /> -->
+              />
               </v-flex>
               <v-flex xs12>
                 <v-textarea
